e5f7dee804aeab9cc03a68525b3b76d2fa77b61d
landreesen <landreesen@uchicago.edu>
2024-06-17 17:10:13 -0400

    replace v1

--- run_csv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source env/bin/activate
-
-# Run the Python script 'main.py' with input from 'urls.csv'
-python3 src/webscraper/scrape_csv.py < urls.csv
